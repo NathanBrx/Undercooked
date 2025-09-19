1b1239b90701de95cf9b0ff8a28219f3f29a29a1
Norazu <nokrani50@gmail.com>
2025-09-19 17:40:10 +0200

    Add : Agent icon update

--- main/path_follow_3d.gd
+++ main/path_follow_3d.gd
@@ -1,18 +1,32 @@
 extends PathFollow3D
 
-@export var step : float = 0.01  # quantité à avancer à chaque “tick”
-@export var interval : float = 0.05  # toutes les 0.5 secondes
-
-func _ready():
-	# Crée un timer pour avancer le chemin toutes les demi-secondes
-	var timer = Timer.new()
-	timer.wait_time = interval
-	timer.one_shot = false
-	timer.autostart = true
-	add_child(timer)
-	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
-
-func _on_timer_timeout():
-	progress_ratio += step
+var checkpoints := [0.0, 0.19, 0.35, 0.61, 0.81]  # positions sur le path (0 à 1)
+var current_checkpoint := 0
+var frames = [
+	null,
+	preload("res://ressources/burger/1.png"),
+	preload("res://ressources/burger/2.png"),
+	preload("res://ressources/burger/3.png"),
+	preload("res://ressources/burger/4.png")
+]
+@export var sprite:Sprite3D
+func _process(delta):
+	# avancer le PathFollow3D
+	progress_ratio += 0.005  # exemple
 	if progress_ratio > 1.0:
-		progress_ratio = 1.0  # ou 0 pour boucler
+		progress_ratio = 1.0
+
+	# vérifier si on a passé le prochain checkpoint
+	if current_checkpoint < checkpoints.size() and progress_ratio >= checkpoints[current_checkpoint]:
+		print("Checkpoint atteint :", current_checkpoint)
+		_on_checkpoint(current_checkpoint)
+		current_checkpoint += 1
+		
+func _on_checkpoint(index):
+	# Exemple : changer la frame du sprite ou ramasser un ingrédient
+	if sprite != null and frames.size() > 0:
+		if index == 0 :
+			sprite.texture = null
+		else :
+			sprite.texture = frames[index]
+	print("Action pour station", index)
