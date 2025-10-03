c44e7171d811b0f14cb0d17c78802e21f0d3df91
Arthur <arthur.de-fays@etu.umontpellier.fr>
2025-10-03 14:43:29 +0200

    Area3D

--- main/path_follow_3d.gd
+++ main/path_follow_3d.gd
@@ -2,6 +2,7 @@ extends PathFollow3D
 
 var checkpoints := [0.0, 0.19, 0.35, 0.61, 0.81]  # positions sur le path (0 à 1)
 var current_checkpoint := 0
+var nameSprite = ""
 var frames = [
 	null,
 	preload("res://ressources/burger/1.png"),
@@ -24,9 +25,51 @@ func _process(delta):
 		
 func _on_checkpoint(index):
 	# Exemple : changer la frame du sprite ou ramasser un ingrédient
-	if sprite != null and frames.size() > 0:
-		if index == 0 :
-			sprite.texture = null
-		else :
-			sprite.texture = frames[index]
-	print("Action pour station", index)
+	print(index)
+	print("Action pour station ", index)
+	print(nameSprite)
+
+
+func _on_area_depot_body_entered(body: Node3D) -> void:
+	current_checkpoint = 0
+	print(current_checkpoint)
+	print("hello")
+	if body.is_in_group("Agents") :
+		sprite.texture = null
+	pass # Replace with function body.
+
+
+func _on_area_steak_body_entered(body: Node3D) -> void:
+	current_checkpoint = 0
+	print(current_checkpoint)
+	print("hello")
+	if body.is_in_group("Agents") :
+		sprite.texture = load("res://ressources/burger/2.png")
+	pass 
+
+
+func _on_area_fromage_body_entered(body: Node3D) -> void:
+	current_checkpoint = 0
+	print(current_checkpoint)
+	print("hello")
+	if body.is_in_group("Agents") :
+		sprite.texture = load("res://ressources/burger/4.png")
+	pass # Replace with function body.
+
+
+func _on_area_salade_body_entered(body: Node3D) -> void:
+	current_checkpoint = 0
+	print(current_checkpoint)
+	print("hello")
+	if body.is_in_group("Agents") :
+		sprite.texture = load("res://ressources/burger/3.png")
+	pass # Replace with function body.
+
+
+func _on_area_pain_body_entered(body: Node3D) -> void:
+	current_checkpoint = 0
+	print(current_checkpoint)
+	print("hello")
+	if body.is_in_group("Agents") :
+		sprite.texture = load("res://ressources/burger/1.png")
+	pass # Replace with function body.
