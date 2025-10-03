5e57321b2df4c867d1e97ce0c9aa050ccd897b84
Norazu <nokrani50@gmail.com>
2025-10-03 15:22:11 +0200

    Add : Code refactor and agent actions

---
+++ main/agent.gd
@@ -0,0 +1,19 @@
+extends CharacterBody3D
+var held_item
+var recette
+var next_action
+func see() :
+	#récupère les états des stations, de la commande
+	pass
+	
+func next() :
+	#execute la prochaine action dans la file, modifie l'état de l'agent
+	pass
+	
+func move_to(station) :
+ #déplace le personnage vers la station correspondante
+	pass
+
+func use_station() :
+	#effectue une action dépendemment de la station dans laquelle on se trouve et de l'inventaire de l'action 
+	pass
