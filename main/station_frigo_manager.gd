342435c2c8572e3b7a508c593d1eda6348eb55b7
Norazu <nokrani50@gmail.com>
2025-10-17 17:16:30 +0200

    frigo fini

--- main/station_frigo_manager.gd
+++ main/station_frigo_manager.gd
@@ -1,6 +1,18 @@
 extends Node3D
 
+var ingredients = ["salade", "steak", "fromage","pain"]
 
 func _on_area_frigo_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Agents") :
-		body.recupere_frigo()
+		body.get_from_fridge(pop_random(ingredients))
+
+func pop_random(array: Array):
+	# Safety check: Can't pop from an empty array.
+	if array.is_empty():
+		return null
+	
+	# Get a random index from 0 to array.size() - 1
+	var random_index = randi() % array.size()
+	
+	# Pop the element at that index and return it
+	return array.pop_at(random_index)
